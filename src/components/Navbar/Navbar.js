3d05456b0ba6747ec20b0e5235ebaf292d187387
KhanhHoang <85919033+koten551@users.noreply.github.com>
2022-04-28 04:46:39 +0700

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ function SuggestNav() {
     const [sugNavExtra, setSugNavExtra] = useState(EX_BTN_TITLE)
     const [maxItem, setMaxItem] = useState(DEFAULT_ITEM_SUGGEST)
     const [suggestList, setSuggestList] = useState([])
-    
+
     useEffect(() => {
         const urlAPI = 'http://localhost:5000/tiktoksuggest'
         fetch(urlAPI)
@@ -23,14 +23,14 @@ function SuggestNav() {
     }, [])
 
     const handleSeeAll = () => {
-        if(sugNav) {
-            if(sugNavExtra === EX_BTN_TITLE) {
+        if (sugNav) {
+            if (sugNavExtra === EX_BTN_TITLE) {
                 setMaxItem(MAX_ITEM_SUGGEST)
                 setSugNavExtra(COMPACT_BTN_TITLE)
-            
+
             } else {
                 setMaxItem(DEFAULT_ITEM_SUGGEST)
-                setSugNavExtra('See all')              
+                setSugNavExtra('See all')
             }
         }
     }
@@ -38,24 +38,24 @@ function SuggestNav() {
     return (
         <div className="suggest-nav__wrap">
             <h4 style={{ color: 'rgba(0,0,0, 0.5)', fontSize: '15px', paddingTop: "14px" }}>Suggested accounts</h4>
-            <ul ref = {sugNav} 
+            <ul ref={sugNav}
                 className="suggest-nav">
                 {suggestList.map((item, index) => {
-                    if(index < maxItem) {
-                        return (
-                            <li key = {index}
-                                className="suggest-nav__item">
-                                <img src={item.avatar} alt="avatar"></img>
-                                <a href='#' style={{textDecoration: 'none', color: '#333'}}>
-                                    <p style={{ fontWeight: "bold", paddingRight: '5px' }}>{item.account}</p>
-                                    <p style={{ fontWeight: "400", color: "rgba(0,0,0, 0.5)", fontSize: "12px", lineHeight: "12px" }}>{item.username}</p>
-                                </a>
-                            </li>
-                        )
-                    }
+
+                    return index < maxItem ? (
+                        <li key={index}
+                            className="suggest-nav__item">
+                            <img src={item.avatar} alt="avatar"></img>
+                            <a href="#1" style={{ textDecoration: 'none', color: '#333' }}>
+                                <p style={{ fontWeight: "bold", paddingRight: '5px' }}>{item.account}</p>
+                                <p style={{ fontWeight: "400", color: "rgba(0,0,0, 0.5)", fontSize: "12px", lineHeight: "12px" }}>{item.username}</p>
+                            </a>
+                        </li>
+                    ) : <></>
+
                 })}
             </ul>
-            <h5  onClick={handleSeeAll}
+            <h5 onClick={handleSeeAll}
                 style={{ color: "var(--primary-color)", paddingTop: "10px", cursor: "pointer" }}>{sugNavExtra}</h5>
         </div>
     )
@@ -67,26 +67,26 @@ function Navbar() {
         <div className="navbar">
             <div className="main-nav">
                 <div className="main-nav__item nav__item-active">
-                    <a href="#">
+                    <a href="#1">
                         <AiFillHome className="nav__item-icon" />
                         For you
                     </a>
                 </div>
                 <div className="main-nav__item">
-                    <a href="#">
+                    <a href="#1">
                         <BsPeople className="nav__item-icon" />
                         Following
                     </a>
                 </div>
                 <div className="main-nav__item">
-                    <a href="#">
+                    <a href="#1">
                         <BsCameraVideo className="nav__item-icon" />
                         LIVE
                     </a>
                 </div>
             </div>
 
-            <SuggestNav/>
+            <SuggestNav />
 
             <div style={{ height: "500px" }}></div>
         </div>
