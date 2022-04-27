72393286b5cfe0a1820f55a0c3241ea8103a3847
KhanhHoang <85919033+koten551@users.noreply.github.com>
2022-04-28 04:46:18 +0700

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
             <div className="wide">
                 <div className="row">
                     <div className="col l-2 m-2 c-2">
-                        <a href="#" style={{ display: "block" }}><img src="./logotiktok.png" alt="logo" className="logo" /> </a>
+                        <a href="#1" style={{ display: "block" }}><img src="./logotiktok.png" alt="logo" className="logo" /> </a>
                     </div>
                     <div className="col l-7 m-7 c-7">
                         <div className="search__box">
@@ -20,7 +20,7 @@ function Header() {
                     <div className="col l-3 m-3 c-3">
                         <div className="right-container">
                             <div>
-                                <a href="#" className="" style={{color: "#333", textDecoration: "none", fontSize: "16px", fontWeight: "bold"}}>Upload</a>
+                                <a href="#1" className="" style={{color: "#333", textDecoration: "none", fontSize: "16px", fontWeight: "bold"}}>Upload</a>
                             </div>
 
                             <div className="login__btn">Log in</div>
